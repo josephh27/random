d2808e0fe2cd587c537c9e7259f850be5ab6bb09
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2021-10-23 12:38:24 +0800

    yeehaw

---
+++ PythonApplication3.py
@@ -0,0 +1,11 @@
+a = input("Enter 1st number")
+x = int(a)
+b = input("Enter 2nd number")
+y = int(b)
+c = input("Enter 3rd number")
+z = int(c)
+result = x + y + z
+print(f"{x} + {y} + {z} = {result}")
+a = 1213
+b = 321
+print(a + b)
